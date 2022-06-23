ccab21c1ade0bae1d4fbe3412b8f603c8576c82b
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-06-23 21:53:47 +0300

    Add filters

---
+++ backend/mortgage_calculator/filters.py
@@ -0,0 +1,32 @@
+from django_filters import NumberFilter, OrderingFilter, FilterSet
+from .utils import calculate_debt
+
+
+class MortgageOfferFilter(FilterSet):
+    term_min = NumberFilter(field_name='term_min', lookup_expr='gte')
+    term_max = NumberFilter(field_name='term_max', lookup_expr='lte')
+    rate_min = NumberFilter(field_name='rate_min', lookup_expr='gte')
+    rate_max = NumberFilter(field_name='rate_max', lookup_expr='lte')
+    order = OrderingFilter(
+        fields=(
+            ('rate_max', 'rate'),
+        ),
+    )
+
+    @property
+    def qs(self):
+        term = self.request.query_params.get('term')
+        price = self.request.query_params.get('price')
+        deposit = self.request.query_params.get('deposit')
+        if (term is not None and price is not None and deposit is not None):
+            debt = calculate_debt(
+                int(price),
+                int(deposit),
+            )
+            return super().qs.filter(
+                term_min__lte=term,
+                term_max__gte=term,
+                payment_min__lte=debt,
+                payment_max__gte=debt,
+            )
+        return super().qs
